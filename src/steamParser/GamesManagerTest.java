a955e9ed1b78ff0f97cba243f05ef3012ca5d00b
Casper Chia <casperchiakyechuen@gmail.com>
2015-06-29 23:43:04 +1000

    Added a test for getGames.

--- src/steamParser/GamesManagerTest.java
+++ src/steamParser/GamesManagerTest.java
@@ -8,7 +8,7 @@ public class GamesManagerTest {
 	public void testLoadGames() throws Exception {
 //		 mozface id
 //		for (int i = 0; i < 20; i++){
-			GamesManager.loadGames("76561198024048520");
+//			GamesManager.loadGames("76561198024048520");
 //		}
 		// eunicell id
 //		GamesManager.loadGames("76561198061965614");
@@ -17,6 +17,8 @@ public class GamesManagerTest {
 //		GamesManager.loadGames((SteamIdManager.getSteamId("ValkyrieBiscuit")));
 
 //		GamesManager.loadAllGames();
+		
+		GamesManager.getGames("76561198024048520");
 	}
  
 }
