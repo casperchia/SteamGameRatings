77e81a07c888743dbec7716ca0344c55e6251653
Casper Chia <casperchiakyechuen@gmail.com>
2015-06-30 16:10:53 +1000

    Add tests for loadGameFromList().

--- src/steamParser/GamesManagerTest.java
+++ src/steamParser/GamesManagerTest.java
@@ -1,5 +1,14 @@
 package steamParser;
 
+import static org.junit.Assert.*;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 public class GamesManagerTest {
@@ -18,7 +27,66 @@ public class GamesManagerTest {
 
 //		GamesManager.loadAllGames();
 		
-		GamesManager.getGames("76561198024048520");
+//		GamesManager.getGames("76561198024048520");
 	}
  
+	@Test
+	public void testLoadGamesFromList() throws Exception {
+		Connection con = null;
+		PreparedStatement pst = null;
+		ResultSet rs = null;
+		
+		try {
+			con = GamesManager.getConnection();
+			con.setAutoCommit(false);
+			pst = con.prepareStatement("DELETE FROM games where appid< 51");
+			pst.executeUpdate();
+			con.commit();
+		} catch (SQLException e) {
+			con.rollback();
+			System.out.println("Connection Failed! Check output console");
+			e.printStackTrace();
+		} finally {
+			if (pst != null) {
+				pst.close();
+			}
+            if (con != null) {
+                con.close();
+            }
+		}
+		
+		List<Integer> appids = new ArrayList<Integer>();
+		appids.add(10);
+		appids.add(20);
+		appids.add(30);
+		appids.add(40);
+		appids.add(50);
+		
+		GamesManager.loadGamesFromList(appids);
+		
+		try {
+			con = GamesManager.getConnection();
+			pst = con.prepareStatement("SELECT * FROM games where appid< 51");
+			rs = pst.executeQuery();
+			
+			for (int i = 10; i < 51; i = i + 10) {
+				rs.next();
+				assertEquals(rs.getInt(1), i);
+			}
+			
+			System.out.println("loadGamesFromList() test successful!");
+			
+		} catch (SQLException e) {
+			System.out.println("Connection Failed! Check output console");
+			e.printStackTrace();
+		} finally {
+			if (pst != null) {
+				pst.close();
+			}
+            if (con != null) {
+                con.close();
+            }
+		}
+	}
+	
 }
